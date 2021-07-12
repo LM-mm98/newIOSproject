41cb1ae17d39f3e1a01d78a8c597f0bb2eda1867
Lin Myat <mac@macs-MacBook-Air.local>
2021-07-12 22:11:35 +0630

    Hello World

---
+++ newIOSproject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  newIOSproject
+//
+//  Created by Lin Myat on 12/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Hello World")
+    }
+
+
+}
+
